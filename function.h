1a53025cb148e9ae8d194b85bb60490118fefb7b
nf0529 <sleepclock78@gmail.com>
2024-11-15 12:50:27 +0900

    +gamerule

--- function.h
+++ function.h
@@ -8,7 +8,20 @@ void player_add(int num) { // 플레이어 추가
 
 }
 
-void gamerule() { // 게임 규칙
-
+void gamerule(){ // 게임 규칙 설명
+    string line;
+    ifstream file("mafiarule.txt");
+    if(file.is_open()){
+        while(getline(file, line)){
+            cout << line << endl;
+        }
+        file.close();
+    }else {
+        cout << "Error to open" << endl;
+    }
+    
 }
 
+void call_player(){
+    
+}
